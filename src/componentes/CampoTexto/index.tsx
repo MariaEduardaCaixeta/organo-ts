1cd747819f557bce4c8493d3b5a06835dce832d9
Maria Eduarda Caixeta <mariaeduarda.caixeta@performait.com>
2026-01-22 16:23:07 +0000

    textField, Colaborator and Team refactor to use TS

---
+++ src/componentes/CampoTexto/index.tsx
@@ -0,0 +1,29 @@
+import './CampoTexto.css'
+
+interface CampoTextoProps {
+    label: string
+    placeholder: string
+    valor: string
+    aoAlterado: (valor: string) => void
+    obrigatorio?: boolean
+}
+
+const CampoTexto = ({label, placeholder, valor, aoAlterado, obrigatorio}: CampoTextoProps) => {
+
+    const placeholderModificada = `${placeholder}...` 
+
+    const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
+        aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {label}
+            </label>
+            <input value={valor} onChange={aoDigitado} required={obrigatorio} placeholder={placeholderModificada}/>
+        </div>
+    )
+}
+
+export default CampoTexto
